1cb65febd5922cd31a685cc13c36b23e443e697b
VLola <valiklolanew@gmail.com>
2023-01-15 17:55:13 +0200

    fixed bugs

--- Project_75/ClientApp/src/components/Symbol.js
+++ Project_75/ClientApp/src/components/Symbol.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import './Symbol.css';
 
 export class Symbol extends Component {
   static displayName = Symbol.name;
 
   constructor(props) {
     super(props);
-    this.state = { symbols: [], loading: true, bets: [], stopLosses: [], selectedBets: [] };
+    this.state = { symbols: [], bets: [], stopLosses: [], selectedBets: [], numbers: [] };
+    this.click = this.click.bind(this);
+    this.add = this.add.bind(this);
   }
 
   componentDidMount() {
     this.populateSymbolData();
-    this.click = this.click.bind(this);
   }
 
+  add(arr) {
+    return arr.reduce((a, b) => a + b, 0);
+  }
 
   async click(){
     let symbol = document.getElementById("selectSymbol").value;
@@ -22,55 +27,67 @@ export class Symbol extends Component {
     let selectedBets = this.state.bets.filter(bet=>bet.symbol === symbol && bet.stopLoss === sl);
     
     this.setState({ selectedBets: selectedBets });
-    
-  }
 
-  static renderSymbolsTable(symbols) {
-    return (
-      <select id='selectSymbol'>
-        {symbols.map(symbol =>
-            <option key={symbol.name} value={symbol.name}>
-              {symbol.name}
-            </option>
-          )}
-      </select>
-    );
   }
 
   render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : Symbol.renderSymbolsTable(this.state.symbols);
-
     return (
       <div>
-        <h1 id="tabelLabel" >Symbols</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-          {contents}
+        <div className='div__flex'>
+          <div className='div__container'>
+            <span>Symbol:</span>
+            <select id='selectSymbol'>
+              {this.state.symbols.map(symbol =>
+                <option key={symbol.name} value={symbol.name}>
+                  {symbol.name}
+                </option>
+              )}
+            </select>
+          </div>
+          <div className='div__container'>
+            <span>StopLoss:</span>
+            <select id='selectStopLoss'>
+              {this.state.stopLosses.map(sl =>
+                <option key={sl} value={sl}>
+                  {sl}
+                </option>
+              )}
+            </select>
+          </div>
+          <div className='div__container'>
+            <button onClick={this.click}>Click</button>
+          </div>
           
-        <select id='selectStopLoss'>
-        {this.state.stopLosses.map(sl =>
-            <option key={sl} value={sl}>
-              {sl}
-            </option>
-          )}
-        </select>
-        <button onClick={this.click}>Click</button>
-        
-        <div>
-          {this.state.selectedBets.map(bet =>
-            <div key={bet.number+bet.symbol+bet.openTime}>
-              <div>{bet.number+bet.symbol}</div>
-              <div>{bet.symbol}</div>
-            </div>
-          )}
         </div>
+        <table className='table table-striped' aria-labelledby="tabelLabel">
+          <thead>
+            <tr>
+              <th>Number</th>
+              <th>Profit</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.numbers.map(number =>
+              <tr key={number}>
+                <td>{number}</td>
+                <td>{this.add(this.state.selectedBets.find(bet=>bet.number === number).map(bet=>bet.profit))}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+          <div>
+            {this.state.selectedBets.map(bet =>
+              <div key={bet.number+bet.symbol+bet.openTime}>
+                <div>{bet.number+bet.symbol}</div>
+                <div>{bet.symbol}</div>
+              </div>
+            )}
+          </div>
       </div>
     );
   }
 
   async populateSymbolData() {
-    console.log("reload");
     const response = await fetch('symbol');
     const data = await response.json();
     
@@ -80,6 +97,10 @@ export class Symbol extends Component {
     const responseSL = await fetch('bet/GetStopLoses');
     const dataStopLoses = await responseSL.json();
 
-    this.setState({ symbols: data , bets: dataBets, stopLosses: dataStopLoses , loading: false });
+    
+    const numbers = dataBets.map(bet=>bet.number);
+    const dataNumbers = Array.from(new Set(numbers));
+
+    this.setState({ symbols: data , bets: dataBets, stopLosses: dataStopLoses, numbers: dataNumbers });
   }
 }
