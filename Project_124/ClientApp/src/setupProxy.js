a5c6adf139f57b202c27fa88f72293793db3a337
VLola <valiklolanew@gmail.com>
2023-03-29 00:20:39 +0300

    fixed bugs

--- Project_124/ClientApp/src/setupProxy.js
+++ Project_124/ClientApp/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:34199';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:7087';
 
 const context =  [
   "/weatherforecast",
